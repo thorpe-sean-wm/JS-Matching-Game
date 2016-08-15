be8e85ec0b92fb467552ab81ec02ed0676a116f9
Sean Thorpe <sean.thorpe@west-mec.org>
2016-08-15 15:20:36 -0700

    Part 2 additions to the page have been added.

---
+++ part2.js
@@ -0,0 +1,11 @@
+function generateFaces() {
+    var numberOfFaces = 5;
+    var theLeftSide = document.getElementById("leftSide");
+    for (i = 0; i < numberOfFaces; ++ i) {
+        squidward_dab = document.createElement("img");
+        squidward_dab.src = "http://ih1.redbubble.net/image.195819531.9489/sticker,375x360.u2.png";
+        squidward_dab.style.top = Math.floor(Math.random() * 401) + "px";
+        squidward_dab.style.left = Math.floor(Math.random() * 401) + "px";
+        theLeftSide.appendChild(squidward_dab);
+    }
+}
